f703c003420c633aeadb75afd48e3c1e00ee4074
RubiaPro <3260644319@qq.com>
2026-07-30 15:05:47 +0800

    Sync complete Sites V15

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+export default function Home() {
+  return (
+    <iframe
+      className="cinematic-site"
+      src="/cinematic.html"
+      title="余圣元｜AI 数据策略师个人展示"
+    />
+  );
+}
